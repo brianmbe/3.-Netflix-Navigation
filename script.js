c30dd47d6c56cf008c10d6fe6cd295c5d943933f
Brian Nyingambe <briannmbe@gmail.com>
2023-02-26 23:03:16 +0300

    creat

---
+++ script.js
@@ -0,0 +1,18 @@
+// Buttons
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+// navs
+const nav = document.querySelectorAll(".nav");
+
+openBtn.addEventListener("click", () => {
+  nav.forEach((navElement) => {
+    navElement.classList.add("visible");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.forEach((navElement) => {
+    navElement.classList.remove("visible");
+  });
+});
